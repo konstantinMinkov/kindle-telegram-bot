290e6dfbd12d69da39cef6e3bb094547b491c36d
Konstantin Minkov <zakedystyle@gmail.com>
2017-05-22 19:03:10 +0300

    initial commit

---
+++ src/test/java/ua/zakedy/controller/TelegramControllerTest.java
@@ -0,0 +1,30 @@
+package ua.zakedy.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TelegramControllerTest {
+
+    @Autowired private MockMvc mvc;
+
+    @Test
+    public void ping() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/ping"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("App is online!")));
+    }
+}
